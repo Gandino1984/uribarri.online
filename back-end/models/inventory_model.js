5c5b015dc336b7f9a92681fe0d5c89b446e72d3a
Gandino1984 <andinogerman@gmail.com>
2024-12-08 13:13:46 +0100

    update

---
+++ back-end/models/inventory_model.js
@@ -0,0 +1,29 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelize.js";
+
+
+const inventory_model = sequelize.define("inventory", {
+    id_inventory: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    id_shop: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    id_provider: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: true
+    },
+    id_product: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+}, {
+    timestamps: true,
+    freezeTableName: true
+});
+
+export default inventory_model;
